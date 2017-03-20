92ae671e8e9c209d89b98bfcae50daa8a1884993
Leonardo Benítez Núñez <leobenitezleo@gmail.com>
2017-03-20 21:22:07 +0100

    Initial commit

---
+++ src/main/java/com/domo/DomoticaApplication.java
@@ -0,0 +1,12 @@
+package com.domo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomoticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomoticaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/domo/DomoticaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.domo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DomoticaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
